fb84079dbc481ab55baa39703c5028de0460d598
Marcos Molero <marcos.molero@worldline.com>
2019-06-13 12:51:49 +0200

    First approach Carthage integration

---
+++ SortingDescriptor/SortingDescriptor.h
@@ -0,0 +1,19 @@
+//
+//  SortingDescriptor.h
+//  SortingDescriptor
+//
+//  Created by tempos21 on 13/06/2019.
+//  Copyright © 2019 Worldline. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SortingDescriptor.
+FOUNDATION_EXPORT double SortingDescriptorVersionNumber;
+
+//! Project version string for SortingDescriptor.
+FOUNDATION_EXPORT const unsigned char SortingDescriptorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SortingDescriptor/PublicHeader.h>
+
+
